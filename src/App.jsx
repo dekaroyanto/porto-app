af2adb5a4581e81f75c98e4b443c3e3613d185bd
Deka Royanto <dekaroy05@gmail.com>
2025-04-09 17:33:20 +0700

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 // import Preloader from "../src/components/Pre";
 import Navbar from "./components/Navbar";
-import Home from "./components/Home/Home";
+import Home from "./components/home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 // import Footer from "./components/Footer";
