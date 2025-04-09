f01139ff07b2a3c95bad72d08a1bb907419267e3
Deka Royanto <dekaroy05@gmail.com>
2025-04-09 17:35:29 +0700

    deploy

--- src/components/Resume/ResumeNew.jsx
+++ src/components/Resume/ResumeNew.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/CV_DEKA.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -22,7 +21,7 @@ function ResumeNew() {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={pdf}
+            href="https://drive.google.com/file/d/1p-RJzpbqWWax1Qg4AkubmdKAJO93HR0K/view"
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
